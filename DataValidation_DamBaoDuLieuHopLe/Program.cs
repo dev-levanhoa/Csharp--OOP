5244189cd484c770c2edb440fb3085de9373ce1a
bruhh <160491492+dev-levanhoa@users.noreply.github.com>
2025-02-24 10:29:13 +0700

    Add files via upload
    Ứng dụng Bao đóng - Đảm bảo dữ liệu hợp lệ

---
+++ DataValidation_DamBaoDuLieuHopLe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataValidation_DamBaoDuLieuHopLe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, password;
+            do
+            {
+                Console.Write("Ten dang nhap : ");
+                username = Console.ReadLine();
+                if (username.Length < 6) Console.WriteLine("Nhap lại !");
+            } while (username.Length < 6);
+            do
+            {
+                Console.Write("Mat khau : ");
+                password = Console.ReadLine();
+                if (password.Length < 8) Console.WriteLine("Nhap lai!");
+            } while (password.Length < 8);
+            User user1 = new User(username, password);
+            Console.WriteLine("Ten dang nhap : " + user1.Username);
+            if(user1.CheckPassword(password))
+                Console.WriteLine("Mat Khau dung !");
+            else
+                Console.WriteLine("Mat khau sai !");
+        }
+    }
+}
---
+++ DataValidation_DamBaoDuLieuHopLe/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataValidation_DamBaoDuLieuHopLe
+{
+    class User
+    {
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                if(value.Length >= 6)
+                {
+                    username = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ten dang nhap phai co it nhat 6 ki tu !");
+                }
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if(value.Length >= 8)
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Mat khau phai co it nhat 8 ki tu!");
+                }
+            }
+        }
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+        public bool CheckPassword(string inputPassword)
+        {
+            return password == inputPassword;
+        }
+    }
+}
